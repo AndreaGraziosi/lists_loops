4cd80e08557cb5b1a9d585b9ea9ac2a58f4576d0
Andrea_Graziosi <andreaggraziosi@gmail.com>
2020-09-29 16:59:12 -0700

    Q3

--- lists_loops.py
+++ lists_loops.py
@@ -1,4 +1,8 @@
 #Q1
 songs = ["ROCKSTAR", "Do It", "For The Night"]
-#Q2
+#Q2  print out "do it"and "for the night"
 print(songs[1:3])
+#Q3 update the last element
+songs[2] = "Miles Away"
+#print the list
+print(songs)
